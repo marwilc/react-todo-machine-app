37d8688fd6491177e80c72f6b933778cf6f9802f
marwilc <marwilcampos@gmail.com>
2023-08-09 23:12:07 -0400

    :sparkles: add appui

---
+++ src/App/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { AppUI } from './AppUI';
+import { useLocalStorage } from './useLocalStorage';
+
+function App() {
+    const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+    const [searchValue, setSearchValue] = React.useState('');
+
+    const completedTodos = todos.filter(
+        (todo) => !!todo.completed
+    ).length;
+    const totalTodos = todos.length;
+
+    const searchedTodos = todos.filter((todo) =>
+        todo.text.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    const completeTodo = (text) => {
+        const newTodos = [...todos];
+        const todoIndex = newTodos.findIndex(
+            (todo) => todo.text === text
+        );
+        newTodos[todoIndex].completed = true;
+        saveTodos(newTodos);
+    };
+
+    const deleteTodo = (text) => {
+        const newTodos = [...todos];
+        const todoIndex = newTodos.findIndex(
+            (todo) => todo.text === text
+        );
+        newTodos.splice(todoIndex, 1);
+        saveTodos(newTodos);
+    };
+
+    console.log(searchValue);
+    return (
+        <AppUI
+            completeTodo={completeTodo}
+            totalTodos={totalTodos}
+            deleteTodo={deleteTodo}
+            searchedTodos={searchedTodos}
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            completedTodos={completedTodos}
+        />
+    );
+}
+
+export default App;
---
+++ src/App/AppUI.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TodoSearch } from '../components/TodoSearch/TodoSearch';
+import { TodoList } from '../components/TodoList/TodoList';
+import { TodoItem } from '../components/TodoItem/TodoItem';
+import { CreateTodoButton } from '../components/CreateTodoButton/CreateTodoButton';
+import { TodoCounter } from '../components/TodoCounter/TodoCounter';
+function AppUI({
+    completeTodo,
+    totalTodos,
+    deleteTodo,
+    searchedTodos,
+    searchValue,
+    setSearchValue,
+    completedTodos,
+}) {
+    return (
+        <>
+            <TodoCounter
+                completed={completedTodos}
+                total={totalTodos}
+            />
+            <TodoSearch
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+            />
+
+            <TodoList>
+                {searchedTodos.map((todo) => (
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
+            </TodoList>
+
+            <CreateTodoButton />
+        </>
+    );
+}
+
+export { AppUI };
